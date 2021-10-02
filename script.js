de44559ba9676e16b3d9bcdf99669e62feb43499
Sahil Dhar <sahildhar98@gmail.com>
2021-10-02 09:46:50 +0530

    styling

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var dateInputRef = document.querySelector('#date-input');
+    var showBtnRef = document.querySelector('#show-btn');
+    var resultRef = document.querySelector('#output');
+
+
+
 function reverseStr(str) {
     var listOfChars = str.split('');
     var reverseListOfChars = listOfChars.reverse();
@@ -60,7 +66,7 @@ function reverseStr(str) {
     return flag;
   }
   
-  // check for leap year
+  
   function isLeapYear(year){
     if(year % 400 === 0){
       return true;
@@ -74,39 +80,39 @@ function reverseStr(str) {
     return false;
   }
   
-  // gets next date
+  
   function getNextDate(date){
-    var day = date.day + 1;  // increment the day  => 32
+    var day = date.day + 1;  
     var month = date.month;
     var year = date.year;
   
-    var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; // 0 - 11
+    var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; 
   
-     // check for february
+     
     if(month === 2){ 
-      // check for leap year
-      if(isLeapYear(year)){ // 2020 => true
-         if(day > 29){ // false
+      
+      if(isLeapYear(year)){
+         if(day > 29){ 
            day = 1;
-           month++;  // increment the month
+           month++;  
          }
       }
     else {
          if(day > 28){
            day = 1;
-           month++;  // increment the month
+           month++;  
          }
       }
     }
     else {
-        //  check if the day exceeds the max days in month
+        
         if(day > daysInMonth[month - 1]){ 
           day = 1; 
-          month++;  // increment the month
+          month++;  
         }
       }
     
-      // increment the year if month is greater than 12
+     
       if(month > 12){
         month = 1;
         year++; 
@@ -119,7 +125,7 @@ function reverseStr(str) {
       };
     }
     
-    // get next palindrome date
+    
     function getNextPalindromeDate(date){
       var ctr = 0;
       var nextDate = getNextDate(date);
@@ -135,16 +141,13 @@ function reverseStr(str) {
       return [ctr, nextDate];
     }
     
-    var dateInputRef = document.querySelector('#date-input');
-    var showBtnRef = document.querySelector('#show-btn');
-    var resultRef = document.querySelector('#output');
+    
     
     function clickHandler(){
       var bdayStr = dateInputRef.value;
       
       if(bdayStr !== ''){
-        var listOfDate = bdayStr.split('-'); // ['2020', '10', '11']
-    
+        var listOfDate = bdayStr.split('-'); 
         var date = {
           day: Number(listOfDate[2]),
           month: Number(listOfDate[1]),
@@ -154,12 +157,12 @@ function reverseStr(str) {
         var isPalindrome = checkPalindromeForAllDateFormats(date);
 
         if(isPalindrome){
-           resultRef.innerText = 'Yay! your birthday is a palindrome!! 🥳🥳';
+           resultRef.innerText = ' Your birthday is a palindrome! ';
         }
         else {
           var [ctr, nextDate] = getNextPalindromeDate(date);
     
-          resultRef.innerText = `The next palindrome date is ${nextDate.day}-${nextDate.month}-${nextDate.year}, you missed it by ${ctr} days! 😔`;
+          resultRef.innerText = `The next palindrome date is ${nextDate.day}-${nextDate.month}-${nextDate.year}, you missed it by ${ctr} days! `;
         }
       }
     }
